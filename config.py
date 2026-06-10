2ab0394b0b6bb1c34e7dbf76a27679d4a4d748c6
Cowsper <casperco96875138@gmail.com>
2026-06-10 15:33:18 +0800

    initialise code for connection to database

---
+++ config.py
@@ -0,0 +1,16 @@
+from configparser import ConfigParser
+
+def config(filename="database.ini", section="postgresql"):
+    # Create parser
+    parser = ConfigParser()
+    # Read config file
+    parser.read(filename)
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        # assign all elements into db dictionary
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section{0} is not found in {1} file'.format(section, filename))
+    return db
--- db_test.py
+++ db_test.py
@@ -1,1 +1,25 @@
+import psycopg2 as ps
+from config import config
 
+def connect():
+    connection = None
+    try:
+        params = config()
+        print('Connection to postgreSQL database')
+        connection = ps.connect(**params)
+        
+        # create cursor
+        crsr = connection.cursor()
+        print('PostgreSQL database version: ')
+        crsr.execute("SELECT version()")
+        db_version = crsr.fetchone()
+        print(db_version)
+        crsr.close()
+    except(Exception, ps.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+            print("DB connection terminated.")
+if __name__ == "__main__":
+    connect()
