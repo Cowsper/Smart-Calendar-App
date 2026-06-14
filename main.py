1e9bf147fe6063514ef6bcf2b1cf5f355d5f6cc5
Cowsper <casperco96875138@gmail.com>
2026-06-14 21:27:38 +0800

    basic activity.py functions created (get, post, delete)

--- main.py
+++ main.py
@@ -14,31 +14,5 @@ def get_db_connection():
 
 app.include_router(users.router)
 app.include_router(events.router)
+app.include_router(activity.router)
 
-
-
-
-# --- ENDPOINT 2: Log a New Activity ---
-@app.post("/activities")
-def create_activity(user_id: int, event_id: int, start_time: datetime, end_time: datetime):
-    # Quick validation before wasting database resources
-    if end_time <= start_time:
-        raise HTTPException(status_code=400, detail="end_time must be after start_time")
-        
-    try:
-        with get_db_connection() as conn:
-            with conn.cursor() as crsr:
-                crsr.execute("""
-                    INSERT INTO activity_table (user_id, event_id, start_time, end_time)
-                    VALUES (%s, %s, %s, %s)
-                    RETURNING activity_id;
-                """, (user_id, event_id, start_time, end_time))
-                
-                conn.commit()
-                
-                return {"message": "Activity logged successfully"}
-                
-    except ps.errors.CheckViolation:
-        raise HTTPException(status_code=400, detail="Database rejected times: end_time must be after start_time")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
--- routers/activity.py
+++ routers/activity.py
@@ -20,14 +20,56 @@ def get_db_connection():
 # Retrieves activities data from the table
 @router.get("/")
 def get_activities():
-    pass
-
+    try: 
+        with get_db_connection() as conn:
+            with conn.cursor() as crsr:
+                crsr.execute("""
+                             SELECT *
+                             FROM activity_table;
+                             """)
+                rows = crsr.fetchall()
+                activities = []
+                for row in rows:
+                    activities.append({
+                        "user_id": row[0],
+                        "event_id": row[1],
+                        "start_time": row[2],
+                        "end_time": row[3],
+                        "activity_id": row[4]
+                    })
+                return activities
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e)
+    
 # Creates an activity in activity_table
 @router.post("/")
-def create_activity():
-    pass
+def create_activity(user_id: int, event_id: int, start_time: datetime, end_time: datetime):
+    if end_time <= start_time:
+        raise HTTPException(status_code=400, detail="start_time must be before end_time.")   
+    try:    
+        with get_db_connection() as conn:
+                with conn.cursor() as crsr:
+                    
+                    crsr.execute("""
+                                INSERT INTO activity_table (user_id, event_id, start_time, end_time)
+                                VALUES (%s, %s, %s, %s)
+                                """, (user_id, event_id, start_time, end_time))
+                    conn.commit()
+                    return {"message": "Activity Successfully Registered!"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 # Deletes an activity in activity_table
 @router.delete("/")
-def delete_activity():
-    pass
+def delete_activity(activity_id: int):
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as crsr:
+                crsr.execute("""
+                             DELETE FROM activity_table
+                             WHERE activity_id = %s;
+                             """, (activity_id,))
+                conn.commit()
+                return {"message": f"{activity_id} successfully deleted."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
